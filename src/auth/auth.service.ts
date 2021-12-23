c74aa92cd46dc911db3012011c1c8ae4a9876e35
Steven Schuette <you@example.com>
2021-12-23 11:59:13 +0100

    Added encryption to passwords

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { Body, Injectable, Post } from '@nestjs/common';
+import { Body, Injectable, Post, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { UserRepository } from './users.repository';
-
+import * as bcrypt from 'bcrypt'
 @Injectable()
 export class AuthService {
   constructor(
@@ -14,4 +14,17 @@ export class AuthService {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
   }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string>{
+      const {username, password} = authCredentialsDto;
+
+      const user = await this.userRepository.findOne({username})
+      if(user && await bcrypt.compare(password, user.password) ){
+        return 'success'
+      }
+      else
+        throw new UnauthorizedException();
+
+  }
+
 }
